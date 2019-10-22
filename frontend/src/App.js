ae2990e0340efc7cc9b1005ba15d2af68933ecf4
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-10-22 17:57:01 -0300

    Pages added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,18 @@
-import React, { useState } from 'react';
-import api from './services/api'
+import React from 'react';
 import './App.css';
 
 import logo from './assets/logo.svg'
 
+import Routes from './routes'
+
 function App() {
-  const [email, setEmail] = useState('')
 
-  async function handleSubmit(e) {
-    e.preventDefault()
-
-    const response = await api.post('/sessions', { email })
-
-  }
 
   return (
     <div className="conteiner">
       <img src={logo} alt="AirCnC" />
       <div className="content">
-        Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Seu melhor e-mail"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
