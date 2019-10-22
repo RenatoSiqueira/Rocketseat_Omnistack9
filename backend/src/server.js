84749bc01259606d3b6ab110c81873918859d22e
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-10-22 17:31:40 -0300

    CORS added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+
 const app = express()
 
 const routes = require('./routes')
@@ -8,11 +10,12 @@ const routes = require('./routes')
 const port = process.env.PORT || 3000
 const MONGOSERVER = process.env.MONGOSERVER || 'mongodb://localhost:27017/omnistack9'
 
+app.use(cors)
+app.use(express.json())
+app.use(routes)
+
 mongoose
     .connect(MONGOSERVER, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((err) => {
-        app.use(express.json())
-        app.use(routes)
-
         app.listen(port, () => console.log('Server Running'))
     })
